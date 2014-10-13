7e8f8609ecd1a301c6b8472a353ef86bdf3832b8
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2014-10-13 23:10:20 +0100

    Create
    Create file.

---
+++ php/home.php
@@ -0,0 +1,24 @@
+
+<html>
+<head>
+	<title>
+	Index
+	</title>
+</head>
+<body>
+<center>
+Bem vindo ao sistema de CAR POOLING!<br>
+	Por favor escolha o seu nick para iniciar sessão...
+<br>
+<br>
+	<form action="home.php" method="POST">
+	  Nick:<select name="login">
+        	<option value ="0" selected>por favor seleccione</option>
+			<option value='rui_'>rui_</option><option value='sofia_'>sofia_</option><option value='andreia_'>andreia_</option><option value='antonio_'>antonio_</option><option value='maria_'>maria_</option><option value='joao_'>joao_</option><option value='alex_'>alex_</option><option value='luis_'>luis_</option>			</select>
+			<br>
+			<input type="submit" value="Iniciar Sessão">
+	</form>
+	
+</center>
+</body>
+</html>
